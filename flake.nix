42b0e588412e201ebaf9c1a2c91301c5355bb0e1
Juuso Haavisto <juuso@ponkila.com>
2023-04-11 18:34:14 +0100

    ponkila-ephemeral-beta: init commit

---
+++ flake.nix
@@ -0,0 +1,85 @@
+# https://xyno.space/post/nix-darwin-introduction
+# https://github.com/Misterio77/nix-starter-configs/tree/main/standard
+# https://sourcegraph.com/github.com/shaunsingh/nix-darwin-dotfiles@8ce14d457f912f59645e167707c4d950ae1c3a6e/-/blob/flake.nix
+{
+
+  inputs = {
+    ethereum-nix.inputs.nixpkgs.follows = "nixpkgs";
+    ethereum-nix.url = "github:nix-community/ethereum.nix";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+
+  # add the inputs declared above to the argument attribute set
+  outputs =
+    { self
+    , ethereum-nix
+    , home-manager
+    , nixos-generators
+    , nixpkgs
+    , sops-nix
+    }@inputs:
+
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      # custom formats for nixos-generators
+      customFormats = {
+        "kexecTree" = {
+          formatAttr = "kexecTree";
+          imports = [ ./system/netboot.nix ];
+        };
+      };
+    in
+    {
+
+      overlays = import ./overlays { inherit inputs; };
+
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
+
+      "ponkila-ephemeral-beta" = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs outputs; };
+        modules = [
+          ./home-manager/core.nix
+          ./hosts/ponkila-ephemeral-beta
+          ./system/ramdisk.nix
+          home-manager.nixosModules.home-manager
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [ ethereum-nix.overlays.default ];
+          })
+          {
+            home-manager.sharedModules = [
+              sops-nix.homeManagerModules.sops
+            ];
+          }
+        ];
+        customFormats = customFormats;
+        format = "kexecTree";
+      };
+
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems
+        (system:
+          let pkgs = nixpkgs.legacyPackages.${system};
+          in import ./shell.nix { inherit pkgs; }
+        );
+
+    };
+}
---
+++ home-manager/core.nix
@@ -0,0 +1,24 @@
+{ inputs
+, config
+, pkgs
+, lib
+, ...
+}:
+{
+
+  users.users.core = {
+    isNormalUser = true;
+    group = "core";
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
+    ];
+    shell = pkgs.fish;
+  };
+  users.groups.core = { };
+  environment.shells = [ pkgs.fish ];
+  programs.fish.enable = true;
+}
