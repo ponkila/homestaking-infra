401827ebe428ced7369e0eb762ecada1b704ceb1
tupakkatapa <jeskar95@outlook.com>
2023-04-18 13:00:42 +0300

    lighthouse: removed version override

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,20 +9,8 @@
   # https://nixos.wiki/wiki/Overlays
   modifications = super: prev: with super.lib; {
     lighthouse = prev.lighthouse.overrideAttrs (old: rec {
-      version = "4.0.2-rc.0";
-      src = super.fetchFromGitHub {
-        owner = "sigp";
-        repo = "lighthouse";
-        rev = "v${version}";
-        # If you don't know the hash, the first time, set:
-        # sha256 = "0000000000000000000000000000000000000000000000000000";
-        sha256 = "sha256-10DpoG9MS6jIod0towzIsmyyakfiT62NIJBKxqsgsK0=";
-      };
       # Enables aggressive optimisations including full LTO
       PROFILE = "maxperf";
-
-      cargoHash = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=";
-
     });
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
