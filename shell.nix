6fa6cf0ac8d82db2e7d555c1ab00bccc30317823
tupakkatapa <jeskar95@outlook.com>
2023-04-18 13:04:13 +0300

    dev-shell: combine with nixpkgs.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,27 @@
-# Shell for bootstrapping flake-enabled nix and home-manager
+# Shell for bootstrapping flake-enabled nix and other tooling
 # You can enter it through 'nix develop' or (legacy) 'nix-shell'
-
-{ pkgs ? (import ./nixpkgs.nix) { } }: {
+{ pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+  import nixpkgs { overlays = [ ]; }
+, ...
+}: {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
-    NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix git sops ];
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+    nativeBuildInputs = with pkgs; [
+      nix
+      home-manager
+      git
+
+      sops
+      gnupg
+      age
+    ];
   };
 }
