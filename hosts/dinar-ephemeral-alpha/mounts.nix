58927432972d329f68b7631a9b01da9ec7e403de
tupakkatapa <jeskar95@outlook.com>
2023-04-19 20:16:50 +0300

    mounts: update

--- hosts/dinar-ephemeral-alpha/mounts.nix
+++ hosts/dinar-ephemeral-alpha/mounts.nix
@@ -1,27 +1,28 @@
-{ disks ? [ "/dev/sda" ], ... }: {
+# sudo nix run github:nix-community/disko -- --mode zap_create_mount ./dinar-disko-config.nix 
+
+{
   disko.devices = {
-    disk = {
-      sda = {
-        type = "disk";
-        device = builtins.elemAt disks 0;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "sda1";
-              start = "1MiB";
-              end = "100%";
-              bootable = false;
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/mnt/var/eth";
-                mountOptions = [ "--label eth" ];
-              };
-            }
-          ];
-        };
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "sda1";
+            start = "1MiB";
+            end = "100%";
+            part-type = "primary";
+            bootable = false;
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/var/mnt/eth";
+              mountOptions = [ "--label eth" ];
+            };
+          }
+        ];
       };
     };
   };
