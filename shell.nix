dc89696496b3689560f7d1a33d9ca16069875ee9
tupakkatapa <jeskar95@outlook.com>
2023-04-20 21:44:47 +0300

    Revert "devshell: remove obselete features"
    This reverts commit 7ba6a952876cdb3f41e9535b8e38e7214eb6dc78.

--- shell.nix
+++ shell.nix
@@ -13,11 +13,15 @@
 }: {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     nativeBuildInputs = with pkgs; [
       nix
+      home-manager
       git
+
       sops
+      gnupg
+      age
     ];
   };
 }
