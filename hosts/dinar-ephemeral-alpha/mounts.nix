74e96aa05144cae07245ee684cc28d2af70a8ffe
tupakkatapa <jeskar95@outlook.com>
2023-04-20 15:40:11 +0300

    mounts: update

--- hosts/dinar-ephemeral-alpha/mounts.nix
+++ hosts/dinar-ephemeral-alpha/mounts.nix
@@ -1,28 +1,53 @@
-# sudo nix run github:nix-community/disko -- --mode zap_create_mount ./dinar-disko-config.nix 
+# sudo nix run github:nix-community/disko -- --mode zap_create_mount ./dinar-disko-config.nix --arg disks '[ "/dev/sda" ]'
 
-{
+{ disks ? [ "/dev/vdb" ], ... }: {
   disko.devices = {
-    disk.sda = {
-      device = "/dev/sda";
-      type = "disk";
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "sda1";
-            start = "1MiB";
-            end = "100%";
-            part-type = "primary";
-            bootable = false;
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/var/mnt/eth";
-              mountOptions = [ "--label eth" ];
-            };
-          }
-        ];
+    disk = {
+      vdb = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "sda1";
+              start = "1MiB";
+              end = "10MiB";
+              bootable = false;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                # systemd should handle mount
+                #mountpoint = "/secrets";
+              };
+            }
+            {
+              name = "sda2";
+              start = "10MiB";
+              end = "50%";
+              bootable = false;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                # systemd should handle mount
+                #mountpoint = "/eth/lighthouse";
+              };
+            }
+            {
+              name = "sda3";
+              start = "50%";
+              end = "100%";
+              bootable = false;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                # systemd should handle mount
+                #mountpoint = "/eth/erigon";
+              };
+            }
+          ];
+        };
       };
     };
   };
