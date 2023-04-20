d165350bea99587260bc26a26f5582575e000313
tupakkatapa <jeskar95@outlook.com>
2023-04-20 19:50:49 +0300

    overlays: refactor comments

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,21 +1,13 @@
-# This file defines overlays
 { inputs, ... }:
 {
-  # This one brings our custom packages from the 'pkgs' directory
+  # Adds custom packages
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  # This one contains whatever you want to overlay
-  # You can change versions, add patches, set compilation flags, anything really.
-  # https://nixos.wiki/wiki/Overlays
-  # final: # package set with all overlays applied, a "fixed" point
-  # prev: # state of the package set before applying this overlay
+  # Modifies existing packages
   modifications = final: prev: {
     lighthouse = prev.lighthouse.overrideAttrs (oldAttrs: rec {
       # Enables aggressive optimisations including full LTO
       PROFILE = "maxperf";
     });
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
   };
 }
