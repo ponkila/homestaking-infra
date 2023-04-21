c84eeb3da2ed2a6b2a15d1a355997417fc8dcf76
tupakkatapa <jeskar95@outlook.com>
2023-04-21 16:42:26 +0300

    eth-duties: wip update

--- pkgs/eth-duties/default.nix
+++ pkgs/eth-duties/default.nix
@@ -1,10 +1,16 @@
+# Change to this after debug
+# { lib
+# , pkgs
+# , poetry2nix
+# , fetchFromGitHub
+# }:
+
 { pkgs ? import <nixpkgs> { }
 , lib ? pkgs.lib
 , poetry2nix ? pkgs.poetry2nix
 , fetchFromGitHub ? pkgs.fetchFromGitHub
 }:
-
-poetry2nix.mkPoetryApplication rec {
+let
   pname = "eth-duties";
   version = "0.3.0";
 
@@ -15,36 +21,65 @@ poetry2nix.mkPoetryApplication rec {
     sha256 = "sha256-CuAbNloZEUMr6nlqk6KG9wqmAD6mPnZ4Xwnv/wpZ70U=";
   };
 
+  python = pkgs.python310;
+  python_slim = python.override {
+    mimetypesSupport = false;
+    x11Support = false;
+    stripConfig = true;
+    stripIdlelib = true;
+    stripTests = true;
+    stripTkinter = true;
+    rebuildBytecode = true;
+    stripBytecode = true;
+    includeSiteCustomize = false;
+    enableOptimizations = false;
+    bzip2 = null;
+    #expat = null;
+    #libffi = null;
+    gdbm = null;
+    xz = null;
+    ncurses = null;
+    #openssl = null;
+    readline = null;
+    sqlite = null;
+    #zlib = null;
+    tzdata = null;
+  };
+in poetry2nix.mkPoetryApplication rec {
+
+  # postPatch = ''
+  #   export HOME=$NIX_BUILD_TOP
+  # '';
+
   projectDir = src;
+  python = python_slim;
+  #preferWheels = true;
 
   # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md
   overrides = poetry2nix.defaultPoetryOverrides.extend (self: super: {
     dataclass-wizard = super.dataclass-wizard.overridePythonAttrs (old: {
       buildInputs = (old.buildInputs or []) ++ [super.setuptools];
     });
-    altgraph = super.altgraph.overridePythonAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [super.setuptools];
-    });
-    poetry-core = super.poetry-core.overridePythonAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [super.poetry-core];
-    });
     isort = super.isort.overridePythonAttrs (old: {
       buildInputs = (old.buildInputs or []) ++ [super.poetry];
     });
-    tomlkit = super.tomlkit.overridePythonAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [super.poetry];
+    altgraph = super.altgraph.overridePythonAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ [super.setuptools];
     });
     pathspec = super.pathspec.overridePythonAttrs (old: {
       buildInputs = (old.buildInputs or []) ++ [super.flit-core];
     });
-    pyinstaller = super.pyinstaller.overridePythonAttrs (old: {
-      buildInputs = (old.buildInputs or []) ++ [pkgs.zlib];
-    });
     pyinstaller-hooks-contrib = super.pyinstaller-hooks-contrib.overridePythonAttrs (old: {
       buildInputs = (old.buildInputs or []) ++ [super.setuptools];
     });
+    # pyinstaller = super.pyinstaller.overridePythonAttrs (old: {
+    #   buildInputs = (old.buildInputs or []) ++ [pkgs.zlib];
+    # });
+    # eth-duties = super.eth-duties.overridePythonAttrs (old: {
+    #   buildInputs = (old.buildInputs or []) ++ [super.poetry-core];
+    # });
   });
-
+  
   meta = with lib; {
     homepage = "https://github.com/TobiWo/eth-duties";
     description = "Tool for logging upcoming validator duties to the console";
@@ -52,4 +87,3 @@ poetry2nix.mkPoetryApplication rec {
     platforms = [ "x86_64-linux" ];
   };
 }
-
