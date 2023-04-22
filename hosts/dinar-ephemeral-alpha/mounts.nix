87d40f3db2c1e966d86e173f9cacafa2c038ad4e
tupakkatapa <jeskar95@outlook.com>
2023-04-22 12:52:21 +0300

    mount: disable mounting

--- hosts/dinar-ephemeral-alpha/mounts.nix
+++ hosts/dinar-ephemeral-alpha/mounts.nix
@@ -3,7 +3,7 @@
 { disks ? [ "/dev/sda" ], ... }: {
   disko.devices = {
     disk = {
-      disk0 = {
+      sda = {
         device = builtins.elemAt disks 0;
         type = "disk";
         content = {
@@ -14,12 +14,14 @@
               name = "sda1";
               start = "1MiB";
               end = "100%";
+              part-type = "primary";
               bootable = false;
               content = {
                 type = "filesystem";
                 format = "ext4";
                 # mountpoint has invisible /mnt prefix 
-                mountpoint = "/eth";
+                # systemd should handle mount
+                #mountpoint = "/eth";
               };
             }
           ];
