fbbe66afea7b5c65119528ffdea75b6f5b27d7a9
tupakkatapa <jeskar95@outlook.com>
2023-05-04 11:52:01 +0000

    erigon: remove dup allowed ports

--- modules/eth/erigon.nix
+++ modules/eth/erigon.nix
@@ -55,11 +55,5 @@ in
       allowedTCPPorts = [ 30303 30304 42069 ];
       allowedUDPPorts = [ 30303 30304 42069 ];
     };
-
-    # firewall
-    networking.firewall = {
-      allowedTCPPorts = [ 30303 30304 42069 ];
-      allowedUDPPorts = [ 30303 30304 42069 ];
-    };
   };
 }
