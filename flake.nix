6d3aea0475ee932aa9ef96d7f01c7815f30d573b
Juuso Haavisto <juuso@ponkila.com>
2023-05-04 21:01:17 +0100

    disable mac builds

--- flake.nix
+++ flake.nix
@@ -44,9 +44,7 @@
     let
       inherit (self) outputs;
       forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-darwin"
         "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
 
