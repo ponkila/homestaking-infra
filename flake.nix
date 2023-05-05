935a5cdfe3dae32a0a963e187167c90b8e58839a
tupakkatapa <jeskar95@outlook.com>
2023-05-05 13:38:23 +0000

    flake: disable darwin at 'forAllSystems'

--- flake.nix
+++ flake.nix
@@ -46,8 +46,8 @@
       forAllSystems = nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
+        #"x86_64-darwin"
+        #"aarch64-darwin"
       ];
 
       # custom formats for nixos-generators
