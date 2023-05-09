8b0c5c69d0090e5326b18668d296459b23023a5f
tupakkatapa <jeskar95@outlook.com>
2023-05-09 01:38:31 +0000

    copytoram: switch to install-iso

--- system/formats/copytoram-iso.nix
+++ system/formats/copytoram-iso.nix
@@ -1,20 +1,20 @@
-# derived from https://github.com/nix-community/nixos-generators/blob/master/formats/iso.nix
-
+# derived from https://github.com/nix-community/nixos-generators/blob/master/formats/iso-install.nix
 { config, lib, modulesPath, ... }:
 {
   imports = [
-    "${toString modulesPath}/installer/cd-dvd/iso-image.nix"
+    "${toString modulesPath}/installer/cd-dvd/installation-cd-base.nix"
   ];
+
+  # for installer
+  isoImage.isoName = "nixos.iso";
+
+  # override installation-cd-base and enable wpa and sshd start at boot
+  systemd.services.wpa_supplicant.wantedBy = lib.mkForce [ "multi-user.target" ];
+  systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
 
   # GRUB timeout
   boot.loader.timeout = lib.mkForce 1;
 
   # Load into a tmpfs during stage-1
   boot.kernelParams = [ "copytoram" ];
-
-  # EFI & USB booting
-  isoImage = {
-    makeEfiBootable = true;
-    makeUsbBootable = true;
-  };
 }
