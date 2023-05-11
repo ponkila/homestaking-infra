3702887c3e1eec63ec8324d149651886929acafd
tupakkatapa <jeskar95@outlook.com>
2023-05-11 20:02:00 +0000

    hetzner-ephemeral-beta: init

--- flake.nix
+++ flake.nix
@@ -101,6 +101,31 @@
         ];
       };
 
+      hetzner-ephemeral-beta = {
+        system = "aarch64-linux";
+        specialArgs = { inherit inputs outputs; };
+        modules = [
+          ./hosts/hetzner-ephemeral-beta
+          ./system/formats/netboot-kexec.nix
+          ./system/global.nix
+          ./system/ramdisk.nix
+          ./home-manager/juuso.nix
+          ./home-manager/kari.nix
+          home-manager.nixosModules.home-manager
+          {
+            nixpkgs.overlays = [
+              outputs.overlays.additions
+              outputs.overlays.modifications
+            ];
+          }
+          {
+            home-manager.sharedModules = [
+              sops-nix.homeManagerModules.sops
+            ];
+          }
+        ];
+      };
+
       dinar-ephemeral-alpha = {
         system = "x86_64-linux";
         specialArgs = { inherit inputs outputs; };
@@ -168,6 +193,7 @@
       packages = forAllSystems (system: {
         dinar-ephemeral-alpha = nixosConfigurations.dinar-ephemeral-alpha.config.system.build.isoImage;
         hetzner-ephemeral-alpha = nixosConfigurations.hetzner-ephemeral-alpha.config.system.build.kexecTree;
+        hetzner-ephemeral-beta = nixosConfigurations.hetzner-ephemeral-beta.config.system.build.kexecTree;
         dinar-ephemeral-beta = nixosConfigurations.dinar-ephemeral-beta.config.system.build.isoImage;
         ponkila-ephemeral-beta = nixosConfigurations.ponkila-ephemeral-beta.config.system.build.kexecTree;
       });
@@ -175,6 +201,7 @@
       nixosConfigurations = with nixpkgs.lib; {
         "dinar-ephemeral-alpha" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] dinar-ephemeral-alpha);
         "hetzner-ephemeral-alpha" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] hetzner-ephemeral-alpha);
+        "hetzner-ephemeral-beta" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] hetzner-ephemeral-beta);
         "dinar-ephemeral-beta" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] dinar-ephemeral-beta);
         "ponkila-ephemeral-beta" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] ponkila-ephemeral-beta);
       };
---
+++ hosts/hetzner-ephemeral-beta/default.nix
@@ -0,0 +1,77 @@
+{ pkgs, config, inputs, lib, ... }:
+{
+  # User options
+  users = {
+    juuso.authorizedKeys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local" ];
+    kari.authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque" ];
+  };
+
+  # Users group
+  users.groups.users = { };
+
+  # Localization
+  networking.hostName = "hetzner-ephemeral-beta";
+  time.timeZone = "Europe/Helsinki";
+
+  # Support for cross compilation
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "armv7l-linux"
+  ];
+
+  # Hercules CI
+  services.hercules-ci-agent = {
+    enable = true;
+    settings.baseDirectory = "/var/mnt/.config/hercules-ci-agent";
+  };
+
+  systemd.mounts = [
+    {
+      enable = true;
+
+      what = "/dev/disk/by-label/nix";
+      where = "/var/mnt/.config";
+      type = "btrfs";
+      options = "subvolid=257";
+
+      before = [ "hercules-ci-agent.service" ];
+      wantedBy = [ "multi-user.target" ];
+    }
+  ];
+
+  systemd.services.nix-remount = {
+    path = [ "/run/wrappers" ];
+    enable = true;
+
+    serviceConfig = {
+      Type = "oneshot";
+    };
+
+    # if a new device, the new .rw-store has to be mounted
+    preStart = ''
+      /run/wrappers/bin/mount /dev/disk/by-label/nix -o subvolid=256 /nix/.rw-store
+    '';
+    script = ''
+      /run/wrappers/bin/mount -t overlay overlay -o lowerdir=/nix/.ro-store:/nix/store,upperdir=/nix/.rw-store/store,workdir=/nix/.rw-store/work /nix/store
+    '';
+
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  # SSH
+  services.openssh = {
+    enable = true;
+    allowSFTP = false;
+    extraConfig = ''
+      AllowTcpForwarding yes
+      X11Forwarding no
+      AllowAgentForwarding no
+      AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
+    '';
+    settings.PasswordAuthentication = false;
+    settings.challengeResponseAuthentication = false;
+  };
+
+  system.stateVersion = "23.05";
+}
