85385ca4f6f14fc967be85c09de674c07412ddc9
tupakkatapa <jeskar95@outlook.com>
2023-05-16 06:19:37 +0000

    flake: use nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     ethereum-nix.url = "github:nix-community/ethereum.nix";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
