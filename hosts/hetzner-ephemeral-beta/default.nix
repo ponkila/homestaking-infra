606524f227de3856a7dd8e754202d21aefd85690
tupakkatapa <jeskar95@outlook.com>
2023-05-16 06:18:05 +0000

    hetzner-ephemeral-beta: mod armv7l support

--- hosts/hetzner-ephemeral-beta/default.nix
+++ hosts/hetzner-ephemeral-beta/default.nix
@@ -13,10 +13,21 @@
   networking.hostName = "hetzner-ephemeral-beta";
   time.timeZone = "Europe/Helsinki";
 
-  # Support for cross compilation
-  boot.binfmt.emulatedSystems = [
-    "armv7l-linux"
+  # Apply kernel personality patch from Ubuntu and configure armv7l support
+  boot.kernelPatches = [
+    rec {
+      name = "compat_uts_machine";
+      patch = pkgs.fetchpatch {
+        inherit name;
+        url = "https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy/patch/?id=c1da50fa6eddad313360249cadcd4905ac9f82ea";
+        sha256 = "sha256-mpq4YLhobWGs+TRKjIjoe5uDiYLVlimqWUCBGFH/zzU=";
+      };
+    }
   ];
+  boot.kernelParams = [
+    "compat_uts_machine=armv7l"
+  ];
+  nix.extraOptions = "extra-platforms = armv7l-linux";
 
   # Hercules CI
   services.hercules-ci-agent = {
