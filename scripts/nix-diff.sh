7c7652be99035b8d8e9952c4989c02721e8d73bd
tupakkatapa <jeskar95@outlook.com>
2023-05-26 00:12:01 +0000

    mission-control init with nix-diff script

---
+++ scripts/nix-diff.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# script for getting diffs between current and main branch nix builds
+# deps: git nix
+# usage: sh ./scripts/nix-diff.sh <hostname>
+
+set -o pipefail
+log_path=/tmp
+hostname=$1
+nix_flags=(
+    --accept-flake-config
+    --extra-experimental-features 'nix-command flakes'
+)
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+buidl() {
+    local hostname=$1
+    local log_prefix=$2
+
+    # Build
+    echo "Building '$hostname' from '$current_branch'.."
+    nix build .#"${hostname}" "${nix_flags[@]}" \
+        --fallback --show-trace -v --log-format raw > "$log_path/${log_prefix}.out.log" 2> "$log_path/${log_prefix}.err.log"
+    
+    # Output nix log and exit if build fails
+    if [ $? -ne 0 ]; then
+        drv=$(grep "For full logs, run" "$log_path/${log_prefix}.err.log" | grep -oE "/nix/store/.*.drv")
+        nix log "$drv"
+        echo "$drv"
+        exit 1
+    fi
+
+    # Get sorted build tree
+    build_path=$(nix path-info --derivation .#"${hostname}" "${nix_flags[@]}" | tail -n 1)
+    echo "Hash: $build_path"
+    nix-store --query --graph "$build_path" \
+        | awk -F'-' '{ print $2, $0 }' | sort -k1 \
+        | cut -d' ' -f2- > "$log_path/${log_prefix}.tree.log"
+
+    # Sep
+    printf '%.s-' {1..69} && echo
+}
+
+# Check if hostname is provided
+if [ -z "$hostname" ]; then
+    echo "Error: Please provide the hostname hostname as an argument."
+    echo "Usage: $0 <hostname>"
+    exit 1
+fi
+
+# Create log directory
+mkdir -p "$log_path" 
+
+# Build from current branch
+buidl "$hostname" "$hostname"
+
+# Build from main branch
+git checkout main > /dev/null
+buidl "$hostname" "$hostname-main"
+
+# Show diffs
+diff_out=$(diff "$log_path/$hostname.tree.log" "$log_path/$hostname-main.tree.log")
+
+if [ -n "$diff_out" ]; then
+  echo "Differences found:"
+  echo "$diff_out" | tee "$log_path/$hostname.diff.log"
+else
+  echo "No differences found."
+fi
+
+# Checkout
+git checkout "$current_branch" > /dev/null 2>&1
