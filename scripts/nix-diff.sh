f08683ac5f88c656f58480a1b4df15b293ad89f6
tupakkatapa <jeskar95@outlook.com>
2023-05-26 06:03:21 +0000

    nix-diff: better output

--- scripts/nix-diff.sh
+++ scripts/nix-diff.sh
@@ -7,7 +7,7 @@ set -o pipefail
 trap cleanup EXIT
 trap cleanup SIGINT
 
-log_path=/tmp
+log_path="/tmp"
 hostname=$1
 nix_flags=(
   --accept-flake-config
@@ -24,28 +24,17 @@ buidl() {
   local hostname=$1
   local log_prefix=$2
 
-  # Build
+  # Build, output nix log and exit if build fails
   echo "Building '$hostname' from '$current_branch'.."
   nix build .#"${hostname}" "${nix_flags[@]}" \
-    --fallback --show-trace -v --log-format raw > "$log_path/${log_prefix}.out.log" 2> "$log_path/${log_prefix}.err.log"
-  
-  # Output nix log and exit if build fails
-  if [ $? -ne 0 ]; then
-    drv=$(grep "For full logs, run" "$log_path/${log_prefix}.err.log" | grep -oE "/nix/store/.*.drv")
-    nix log "$drv"
-    echo "$drv"
-    exit 1
-  fi
+    --fallback --show-trace -v --log-format raw > >(tee "$log_path/$log_prefix.out.log") 2> >(tee "$log_path/$log_prefix.err.log" >&2) \
+    || exit 1
 
   # Get sorted build tree
   build_path=$(nix path-info --derivation .#"${hostname}" "${nix_flags[@]}" | tail -n 1)
-  echo "Hash: $build_path"
   nix-store --query --graph "$build_path" \
     | awk -F'-' '{ print $2, $0 }' | sort -k1 \
     | cut -d' ' -f2- > "$log_path/${log_prefix}.tree.log"
-
-  # Sep
-  printf '%.s-' {1..69} && echo
 }
 
 # Check if hostname is provided
@@ -70,12 +59,13 @@ git clone "$remote_url" --branch main --single-branch "$temp_dir"
 cd "$temp_dir" || exit 1
 buidl "$hostname" "$hostname-main"
 
-# Show diffs
+# Result
+printf '%.s-' {1..69} && echo
 diff_out=$(diff "$log_path/$hostname.tree.log" "$log_path/$hostname-main.tree.log")
 
 if [ -n "$diff_out" ]; then
-  echo "Differences found:"
-  echo "$diff_out" | tee "$log_path/$hostname.diff.log"
+  echo "$diff_out" > "$log_path/$hostname.diff.log"
+  echo "Differences found: '$log_path/$hostname.diff.log'"
 else
   echo "No differences found."
 fi
