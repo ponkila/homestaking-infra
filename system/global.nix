3e3e5759f31d52c6989161fb5d734f048bc6c246
tupakkatapa <jeskar95@outlook.com>
2023-05-28 17:48:35 +0000

    global: increase tmpfs size 30%

--- system/global.nix
+++ system/global.nix
@@ -23,23 +23,32 @@
     package = pkgs.nix;
   };
 
-  boot.kernelParams = [
-    "boot.shell_on_fail"
+  boot = {
+    kernelParams = [
+      "boot.shell_on_fail"
 
-    "mitigations=off"
-    "l1tf=off"
-    "mds=off"
-    "no_stf_barrier"
-    "noibpb"
-    "noibrs"
-    "nopti"
-    "nospec_store_bypass_disable"
-    "nospectre_v1"
-    "nospectre_v2"
-    "tsx=on"
-    "tsx_async_abort=off"
-  ];
-  boot.kernelPackages = lib.mkDefault (pkgs.linuxPackagesFor (pkgs.linux_latest));
+      "mitigations=off"
+      "l1tf=off"
+      "mds=off"
+      "no_stf_barrier"
+      "noibpb"
+      "noibrs"
+      "nopti"
+      "nospec_store_bypass_disable"
+      "nospectre_v1"
+      "nospectre_v2"
+      "tsx=on"
+      "tsx_async_abort=off"
+    ];
+    kernelPackages = lib.mkDefault (pkgs.linuxPackagesFor (pkgs.linux_latest));
+  } // (
+    # Increase tmpfs (default: "50%")
+    if (lib.trivial.release == "22.11") then {
+      tmpOnTmpfsSize = "80%";
+    } else {
+      tmp.tmpfsSize = "80%";
+    }
+  );
 
   environment.systemPackages = with pkgs; [
     btrfs-progs
