f936275a0cc4d1489198b65c017cd624d90fcac1
tupakkatapa <jeskar95@outlook.com>
2023-06-05 08:01:20 +0000

    ponkila-gamma: append to comment

--- flake.nix
+++ flake.nix
@@ -150,6 +150,7 @@
                   outputs.overlays.additions
                   outputs.overlays.modifications
                   # Workaround for https://github.com/NixOS/nixpkgs/issues/154163
+                  # This issue only happens with the isoImage format
                   (final: super: {
                     makeModulesClosure = x:
                       super.makeModulesClosure (x // { allowMissing = true; });
