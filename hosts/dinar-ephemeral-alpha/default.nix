ba5d33a0a589c7ceb3dd5131afc242c02d23af76
tupakkatapa <jeskar95@outlook.com>
2023-06-08 00:46:44 +0000

    eth-hosts: update homestakeros options

--- hosts/dinar-ephemeral-alpha/default.nix
+++ hosts/dinar-ephemeral-alpha/default.nix
@@ -6,46 +6,69 @@ let
   sshKeysPath = "/mnt/eth/secrets/ssh/id_ed25519";
 in
 {
-  # User options
-  user = {
-    authorizedKeys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
-    ];
-  };
-
-  # Localization options
-  localization = {
-    hostname = "dinar-ephemeral-alpha";
-    timezone = "Europe/Helsinki";
-  };
-
   # Use stable kernel
   boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux);
 
-  # Erigon options
-  erigon = {
-    enable = true;
-    endpoint = "http://${infra.ip}:8551";
-    datadir = "/mnt/eth/erigon";
-  };
-
-  # Lighthouse options
-  lighthouse = {
-    enable = true;
-    endpoint = "http://${infra.ip}:5052";
-    datadir = "/mnt/eth/lighthouse";
-    exec.endpoint = "http://${infra.ip}:8551";
-    mev-boost.endpoint = "http://${infra.ip}:18550";
-    slasher = {
-      enable = false;
-      history-length = 256;
-      max-db-size = 16;
+  homestakeros = {
+    # Localization options
+    localization = {
+      hostname = "dinar-ephemeral-alpha";
+      timezone = "Europe/Helsinki";
     };
+
+    # User options
+    user = {
+      authorizedKeys = [
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
+      ];
+    };
+
+    # SSH (system level) options
+    ssh = {
+      privateKeyPath = sshKeysPath;
+    };
+
+    # Erigon options
+    erigon = {
+      enable = true;
+      endpoint = "http://${infra.ip}:8551";
+      datadir = "/mnt/eth/erigon";
+    };
+
+    # Lighthouse options
+    lighthouse = {
+      enable = true;
+      endpoint = "http://${infra.ip}:5052";
+      datadir = "/mnt/eth/lighthouse";
+      exec.endpoint = "http://${infra.ip}:8551";
+      mev-boost.endpoint = "http://${infra.ip}:18550";
+      slasher = {
+        enable = false;
+        history-length = 256;
+        max-db-size = 16;
+      };
+    };
+
+    # Mounts
+    mounts = [
+      {
+        enable = true;
+        description = "storage";
+
+        what = "/dev/sda1";
+        where = "/mnt/eth";
+        type = "ext4";
+
+        before = [ "sops-nix.service" "sshd.service" ];
+        wantedBy = [ "multi-user.target" ];
+      }
+    ];
   };
 
+  # Secrets
   home-manager.users = {
     root = { pkgs, ... }: {
       sops = {
@@ -71,79 +94,5 @@ in
     };
   };
 
-  # SSH (system level) options
-  ssh = {
-    privateKeyPath = "/var/mnt/secrets/ssh/id_ed25519";
-  };
-
-  # Mounts
-  mounts = [
-    {
-      enable = true;
-      description = "storage";
-
-      what = "/dev/sda1";
-      where = "/mnt/eth";
-      type = "ext4";
-
-      before = [ "sops-nix.service" "sshd.service" ];
-      wantedBy = [ "multi-user.target" ];
-    }
-  ];
-
-  # SSH
-  services.openssh = {
-    enable = true;
-    hostKeys = [{
-      path = sshKeysPath;
-      type = "ed25519";
-    }];
-    allowSFTP = false;
-    extraConfig = ''
-      AllowTcpForwarding yes
-      X11Forwarding no
-      AllowAgentForwarding no
-      AllowStreamLocalForwarding no
-      AuthenticationMethods publickey
-    '';
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
-
-  # Prometheus
-  services.prometheus = {
-    enable = false;
-    port = 9001;
-    exporters = {
-      node = {
-        enable = false;
-        enabledCollectors = [ "systemd" ];
-        port = 9002;
-      };
-    };
-    scrapeConfigs = [
-      {
-        job_name = config.networking.hostName;
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-      {
-        job_name = "erigon";
-        metrics_path = "/debug/metrics/prometheus";
-        scheme = "http";
-        static_configs = [{
-          targets = [ "127.0.0.1:6060" "127.0.0.1:6061" "127.0.0.1:6062" ];
-        }];
-      }
-      {
-        job_name = "lighthouse";
-        scrape_interval = "5s";
-        static_configs = [{
-          targets = [ "127.0.0.1:5054" "127.0.0.1:5064" ];
-        }];
-      }
-    ];
-  };
   system.stateVersion = "23.05";
 }
--- hosts/ponkila-ephemeral-beta/default.nix
+++ hosts/ponkila-ephemeral-beta/default.nix
@@ -8,41 +8,88 @@ let
   sshKeysPath = "/var/mnt/secrets/ssh/id_ed25519";
 in
 {
-  # User options
-  user = {
-    authorizedKeys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
-    ];
-  };
-
-  # Localization options
-  localization = {
-    hostname = "ponkila-ephemeral-beta";
-    timezone = "Europe/Helsinki";
-  };
-
-  # Erigon options
-  erigon = {
-    enable = true;
-    endpoint = "http://${infra.ip}:8551";
-    datadir = erigon.datadir;
-  };
-
-  # Lighthouse options
-  lighthouse = {
-    enable = true;
-    endpoint = "http://${infra.ip}:5052";
-    datadir = lighthouse.datadir;
-    exec.endpoint = "http://${infra.ip}:8551";
-    mev-boost.endpoint = "http://${infra.ip}:18550";
-    slasher = {
-      enable = false;
-      history-length = 256;
-      max-db-size = 16;
+  homestakeros = {
+    # Localization options
+    localization = {
+      hostname = "ponkila-ephemeral-beta";
+      timezone = "Europe/Helsinki";
     };
+
+    # User options
+    user = {
+      authorizedKeys = [
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
+      ];
+    };
+
+    # SSH (system level) options
+    ssh = {
+      privateKeyPath = sshKeysPath;
+    };
+
+    # Erigon options
+    erigon = {
+      enable = true;
+      endpoint = "http://${infra.ip}:8551";
+      datadir = erigon.datadir;
+    };
+
+    # Lighthouse options
+    lighthouse = {
+      enable = true;
+      endpoint = "http://${infra.ip}:5052";
+      datadir = lighthouse.datadir;
+      exec.endpoint = "http://${infra.ip}:8551";
+      mev-boost.endpoint = "http://${infra.ip}:18550";
+      slasher = {
+        enable = false;
+        history-length = 256;
+        max-db-size = 16;
+      };
+    };
+
+    # Mount options
+    mounts = [
+      # Secrets
+      {
+        enable = true;
+        description = "secrets storage";
+
+        what = "/dev/disk/by-label/secrets";
+        where = "/var/mnt/secrets";
+        type = "btrfs";
+
+        before = [ "sshd.service" ];
+        wantedBy = [ "multi-user.target" ];
+      }
+      # Erigon
+      {
+        enable = true;
+        description = "erigon storage";
+
+        what = "/dev/disk/by-label/erigon";
+        where = erigon.datadir;
+        options = "noatime";
+        type = "btrfs";
+
+        wantedBy = [ "multi-user.target" ];
+      }
+      # Lighthouse
+      {
+        enable = true;
+        description = "lighthouse storage";
+
+        what = "/dev/disk/by-label/lighthouse";
+        where = lighthouse.datadir;
+        options = "noatime";
+        type = "btrfs";
+
+        wantedBy = [ "multi-user.target" ];
+      }
+    ];
   };
 
   # Secrets
@@ -54,106 +101,6 @@ in
       };
       age.sshKeyPaths = [ sshKeysPath ];
     };
-  };
-
-  # SSH (system level) options
-  ssh = {
-    privateKeyPath = "/var/mnt/secrets/ssh/id_ed25519";
-  };
-
-  # Mount options
-  mounts = [
-    # Secrets
-    {
-      enable = true;
-      description = "secrets storage";
-
-      what = "/dev/disk/by-label/secrets";
-      where = "/var/mnt/secrets";
-      type = "btrfs";
-
-      before = [ "sshd.service" ];
-      wantedBy = [ "multi-user.target" ];
-    }
-    # Erigon
-    {
-      enable = true;
-      description = "erigon storage";
-
-      what = "/dev/disk/by-label/erigon";
-      where = erigon.datadir;
-      options = "noatime";
-      type = "btrfs";
-
-      wantedBy = [ "multi-user.target" ];
-    }
-    # Lighthouse
-    {
-      enable = true;
-      description = "lighthouse storage";
-
-      what = "/dev/disk/by-label/lighthouse";
-      where = lighthouse.datadir;
-      options = "noatime";
-      type = "btrfs";
-
-      wantedBy = [ "multi-user.target" ];
-    }
-  ];
-
-  # SSH
-  services.openssh = {
-    enable = true;
-    hostKeys = [{
-      path = sshKeysPath;
-      type = "ed25519";
-    }];
-    allowSFTP = false;
-    extraConfig = ''
-      AllowTcpForwarding yes
-      X11Forwarding no
-      AllowAgentForwarding no
-      AllowStreamLocalForwarding no
-      AuthenticationMethods publickey
-    '';
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-  };
-
-  # Prometheus
-  services.prometheus = {
-    enable = false;
-    port = 9001;
-    exporters = {
-      node = {
-        enable = false;
-        enabledCollectors = [ "systemd" ];
-        port = 9002;
-      };
-    };
-    scrapeConfigs = [
-      {
-        job_name = config.networking.hostName;
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-      {
-        job_name = "erigon";
-        metrics_path = "/debug/metrics/prometheus";
-        scheme = "http";
-        static_configs = [{
-          targets = [ "127.0.0.1:6060" "127.0.0.1:6061" "127.0.0.1:6062" ];
-        }];
-      }
-      {
-        job_name = "lighthouse";
-        scrape_interval = "5s";
-        static_configs = [{
-          targets = [ "127.0.0.1:5054" "127.0.0.1:5064" ];
-        }];
-      }
-    ];
   };
 
   # Enable an ONC RPC directory service used by NFS
