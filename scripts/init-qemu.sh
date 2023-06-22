a255f6e25c7665d688bda5e4c5b544491bfaf99e
Tupakkatapa <jeskar95@outlook.com>
2023-06-22 12:56:56 +0300

    small improvements and refactoring

--- scripts/init-qemu.sh
+++ scripts/init-qemu.sh
@@ -9,14 +9,14 @@ script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 hostname=$1
 
 # Check if argument was given
-if [ $# -eq 0 ]; then
+if [ $# -ne 1 ]; then
     echo "No hostname were provided."
     exit 1
 fi
 
 # Check if QEMU is installed
 if ! command -v qemu-system-x86_64 &>/dev/null; then
-    echo "QEMU is not found. Please install QEMU."
+    echo "qemu-system-x86_64: command not found. Please install QEMU."
     exit 1
 fi
 
@@ -26,20 +26,27 @@ if [ ! -d "$script_dir/../hosts/$hostname" ]; then
   exit 1
 fi
 
-# Check if the host is kexecTree format
+# Check if the host uses the kexecTree format
 if [[ -z $(nix eval --json .#nixosConfigurations."$hostname".config.system.build.kexecTree 2>/dev/null) ]]; then
     echo "$hostname does not use the kexecTree format."
     exit 1
 fi
 
 # Build the host
-nix build .#"$hostname"
+nix build .#"$hostname" || exit 1
 
 # Get kernel args from kexec-boot script
 kernel_args=$(sed -n '/--command-line/p' "$script_dir/../result/kexec-boot" | cut -d\" -f2)
 
 # Launch QEMU
 qemu-system-x86_64 \
-    -kernel "$script_dir/../result/bzImage" \
-    -initrd "$script_dir/../result/initrd.zst" \
-    -append "$kernel_args" -m 8192M
+  -kernel "$script_dir/../result/bzImage" \
+  -initrd "$script_dir/../result/initrd.zst" \
+  -append "$kernel_args" -m 8192M
+
+# qemu-system-aarch64 \
+#   -M virt -m 8192M -cpu cortex-a53 \
+#   -kernel "$script_dir/../result/bzImage" \
+#   -initrd "$script_dir/../result/initrd.zst" \
+#   -append "console=ttyAMA0,38400n8 rdinit=/bin/sh" \
+#   -nographic
