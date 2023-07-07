2556323c8f9310eab22d1b6ac049ccd41e8281c9
Tupakkatapa <jeskar95@outlook.com>
2023-07-08 00:01:33 +0300

    hosts: update homestakeros opts

--- hosts/ponkila-ephemeral-beta/default.nix
+++ hosts/ponkila-ephemeral-beta/default.nix
@@ -34,13 +34,13 @@ in
     };
 
     # Wireguard options
-    wireguard = {
+    vpn.wireguard = {
       enable = true;
       configFile = config.sops.secrets."wireguard/wg0".path;
     };
 
     # Erigon options
-    erigon = {
+    execution.erigon = {
       enable = true;
       endpoint = "http://${infra.ip}:8551";
       dataDir = erigon.datadir;
@@ -48,20 +48,22 @@ in
     };
 
     # Lighthouse options
-    lighthouse = {
+    consensus.lighthouse = {
       enable = true;
       endpoint = "http://${infra.ip}:5052";
       dataDir = lighthouse.datadir;
-      mev-boost = {
-        enable = true;
-        endpoint = "${infra.ip}:18550";
-      };
       slasher = {
         enable = false;
         historyLength = 256;
         maxDatabaseSize = 16;
       };
       jwtSecretFile = "/var/mnt/lighthouse/jwt.hex";
+    };
+
+    # Addons
+    addons.mev-boost = {
+      enable = true;
+      endpoint = "http://${infra.ip}:18550";
     };
 
     # Mount options
