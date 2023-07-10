2bb495c928c5395be64c7f113e16a34055a1829d
Tupakkatapa <jeskar95@outlook.com>
2023-07-10 06:28:58 +0300

    flake: add x86_64-darwin to systems

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
       systems = [
         "aarch64-darwin"
         "aarch64-linux"
+        "x86_64-darwin"
         "x86_64-linux"
       ];
       perSystem = { pkgs, lib, config, system, ... }: {
