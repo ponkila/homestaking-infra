184eca369f79b31cb5046de069bfec48f01b37bb
tupakkatapa <jeskar95outlook.com>
2023-07-20 18:22:13 +0300

    Revert "hetzner-ephemeral-alpha: possible nix-serve fix 3"
    This reverts commit b49108dd21199c9e5a944d17a54e82ada68e0b02.

--- hosts/hetzner-ephemeral-alpha/default.nix
+++ hosts/hetzner-ephemeral-alpha/default.nix
@@ -112,7 +112,6 @@ in
   # Secrets
   sops = {
     secrets."cache-server/private-key" = {
-      owner = "root";
       sopsFile = ./secrets/default.yaml;
     };
     age.sshKeyPaths = [ sshKeysPath ];
