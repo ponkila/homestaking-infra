8330b4c39aad400e5bf17469d68cfa2cf8baca00
tupakkatapa <jesse@ponkila.com>
2023-10-06 19:32:35 +0300

    remove buidl1.ponkila.com

--- flake.nix
+++ flake.nix
@@ -9,13 +9,11 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "http://buidl0.ponkila.com:5000"
-      "http://buidl1.ponkila.com:5000"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "buidl0.ponkila.com:qJZUo9Aji8cTc0v6hIGqbWT8sy+IT/rmSKUFTfhVGGw="
-      "buidl1.ponkila.com:ZIIETN3bdTS4DtymDmVGKqG6UOPy4gU89DPCfAKDcx8="
     ];
   };
 
