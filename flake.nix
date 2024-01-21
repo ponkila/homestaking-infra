61178e47e650ce0413415b03966c7f3ca544d532
tupakkatapa <jesse@ponkila.com>
2024-01-21 22:18:49 +0200

    add rpi4 bootloader back

--- flake.nix
+++ flake.nix
@@ -154,6 +154,11 @@
                     super.makeModulesClosure (x // {allowMissing = true;});
                 })
               ];
+              # Bootloader for RaspberryPi 4
+              boot.loader.raspberryPi = {
+                enable = true;
+                version = 4;
+              };
               boot.loader.grub.enable = false;
             }
           ];
