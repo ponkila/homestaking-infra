0a614db2b6daeabb33c720e531f1060ac6d507b7
Afrikantahti <46537131+Afrikantahti@users.noreply.github.com>
2024-03-23 22:30:49 +0200

    Update default.nix
    gelf to promtail
    remove ssh, lighthouse, mevboost and erigon.

--- nixosConfigurations/dinar-ephemeral-alpha/default.nix
+++ nixosConfigurations/dinar-ephemeral-alpha/default.nix
@@ -15,52 +15,12 @@ in {
       timezone = "Europe/Helsinki";
     };
 
-    # SSH options
-    ssh = {
-      authorizedKeys = [
-        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAID5aw7sqJrXdKdNVu9IAyCCw1OYHXFQmFu/s/K+GAmGfAAAABHNzaDo= da@pusu"
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINwWpZR5WuzyJlr7jYoe0mAYp+MJ12doozfqGz9/8NP/AAAABHNzaDo= da@pusu"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkfHIgiK8S5awFn+oOdduS2mp5UGT4ki/ndoMArBol1dvRSKAdHS4okCX/umiy4BqAsDFkpYWuwe897NdOosba0iVyrFsYRou9FrOnQIMRIgtAvaOXeo2U4432glzH4WsMD+D+F4wHZ7walsrkaIPihpoHtWp8DkTPcFm1D8GP1o5TNpTjSFSuPFSzC2nburVcyfxZJluh/hxnxtYLNrmwOOHLhXcTmy5rQQ5u2HI5y64tS6fnKxxozA2gPaVro5+W5e3WtpSDGdd2NkPDzrMMmwYFEv4Tw9ooUfaJhXhq7AJakK/nTfpLquL9XSia8af+aOzx/p1v25f56dESlhNzcSlREP52hTA9T3foCA2IBkDitBeeGhUeeerQdczoRFxxSjoI244bPwAZ+tKIwO0XFaxLyd3jjzlya0F9w1N7wN0ZO4hY1NVv7oaYTUcU7TnvqGEMGLZpQBnIn7DCrUjKeW4AIUGvxcCP+F16lqFkuLSCgOAHM59NECVwBAOPGDk="
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdbU8l66hVUAqk900GmEme5uhWcs05JMUQv2eD0j7MI juuso@starlabs"
-      ];
-      privateKeyFile = "/mnt/eth/ssh/id_ed25519";
-    };
-
     # Wireguard options
     vpn.wireguard = {
       enable = true;
       configFile = "/mnt/eth/wg0.conf";
     };
 
-    # Erigon options
-    execution.erigon = {
-      enable = true;
-      endpoint = "http://${infra.ip}:8551";
-      dataDir = "/mnt/eth/erigon";
-      jwtSecretFile = "/mnt/eth/jwt.hex";
-    };
-
-    # Lighthouse options
-    consensus.lighthouse = {
-      enable = true;
-      endpoint = "http://${infra.ip}:5052";
-      execEndpoint = "http://${infra.ip}:8551";
-      dataDir = "/mnt/eth/lighthouse";
-      slasher = {
-        enable = false;
-        historyLength = 256;
-        maxDatabaseSize = 16;
-      };
-      jwtSecretFile = "/mnt/eth/jwt.hex";
-    };
-
-    # Addons
-    addons.mev-boost = {
-      enable = true;
-      endpoint = "http://${infra.ip}:18550";
-    };
     addons.ssv-node = {
       dataDir = "/mnt/eth/ssv";
       privateKeyFile = "/mnt/eth/ssv/ssv_operator_key";
@@ -136,10 +96,39 @@ in {
   };
   security.auditd.enable = true;
 
-  services.SystemdJournal2Gelf = {
+  services.promtail = {
     enable = true;
-    graylogServer = "192.168.250.15:12201";
-    extraOptions = "--follow";
+    configuration = {
+      server = {
+        http_listen_port = 9080;
+        grpc_listen_port = 0;
+      };
+      positions = {
+        filename = "/tmp/positions.yaml";
+      };
+      clients = [{
+        url = "url: http://192.168.250.200:3100/loki/api/v1/push";
+      }];
+      scrape_configs = [{
+        job_name = "journal";
+        journal = {
+          max_age = "12h";
+          labels = {
+            job = "systemd-journal";
+            host = "pihole";
+          };
+        };
+        relabel_configs = [{
+          source_labels = [ "__journal__systemd_unit" ];
+          target_label = "unit";
+          source_labels = [ "__journal__hostname" ];
+          target_label = "hostname"; 
+          source_labels = [ "__journal_priority_keyword" ];
+          target_label = "level";
+        }];
+      }];
+    };
+    # extraFlags
   };
 
   services.netdata.enable = true;
