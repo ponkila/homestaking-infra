eab63635cf3bf1b563e12fba13b10886b860b4fb
Juuso Haavisto <juuso@ponkila.com>
2024-04-07 00:00:59 +0100

    flake: update to main

--- flake.nix
+++ flake.nix
@@ -21,8 +21,8 @@
     devenv.url = "github:cachix/devenv";
     flake-parts.url = "github:hercules-ci/flake-parts";
     homestakeros-base.inputs.nixpkgs.follows = "nixpkgs";
-    homestakeros-base.url = "github:ponkila/HomestakerOS/jesse/mv-module-here?dir=nixosModules/base";
-    homestakeros.url = "github:ponkila/HomestakerOS/jesse/mv-module-here";
+    homestakeros-base.url = "github:ponkila/HomestakerOS?dir=nixosModules/base";
+    homestakeros.url = "github:ponkila/HomestakerOS";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
