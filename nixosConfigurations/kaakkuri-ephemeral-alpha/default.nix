c97e32f025acc2e584293a46fcc5a79c512ca24a
Juuso Haavisto <juuso@ponkila.com>
2024-08-09 19:46:01 +0100

    kaakkuri-ephemeral-alpha: update fs layout to lvm+xfs

--- nixosConfigurations/kaakkuri-ephemeral-alpha/default.nix
+++ nixosConfigurations/kaakkuri-ephemeral-alpha/default.nix
@@ -1,5 +1,18 @@
-_:
+{ lib
+, pkgs
+, ...
+}:
+let
+  sshKeysPath = "/var/mnt/ssd/secrets/ssh/id_ed25519";
+in
 {
+  boot.initrd.availableKernelModules = [ "xfs" ];
+  fileSystems."/var/mnt/ssd" = lib.mkImageMediaOverride {
+    fsType = "xfs";
+    device = "/dev/mapper/samsung-ssd";
+    neededForBoot = true;
+  };
+
   homestakeros = {
     # Localization options
     localization = {
@@ -17,30 +30,64 @@ _:
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkfHIgiK8S5awFn+oOdduS2mp5UGT4ki/ndoMArBol1dvRSKAdHS4okCX/umiy4BqAsDFkpYWuwe897NdOosba0iVyrFsYRou9FrOnQIMRIgtAvaOXeo2U4432glzH4WsMD+D+F4wHZ7walsrkaIPihpoHtWp8DkTPcFm1D8GP1o5TNpTjSFSuPFSzC2nburVcyfxZJluh/hxnxtYLNrmwOOHLhXcTmy5rQQ5u2HI5y64tS6fnKxxozA2gPaVro5+W5e3WtpSDGdd2NkPDzrMMmwYFEv4Tw9ooUfaJhXhq7AJakK/nTfpLquL9XSia8af+aOzx/p1v25f56dESlhNzcSlREP52hTA9T3foCA2IBkDitBeeGhUeeerQdczoRFxxSjoI244bPwAZ+tKIwO0XFaxLyd3jjzlya0F9w1N7wN0ZO4hY1NVv7oaYTUcU7TnvqGEMGLZpQBnIn7DCrUjKeW4AIUGvxcCP+F16lqFkuLSCgOAHM59NECVwBAOPGDk="
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdbU8l66hVUAqk900GmEme5uhWcs05JMUQv2eD0j7MI juuso@starlabs"
       ];
-      privateKeyFile = "/mnt/secrets/ssh/id_ed25519";
+      privateKeyFile = sshKeysPath;
     };
 
     # Wireguard options
     vpn.wireguard = {
       enable = true;
-      configFile = "/mnt/secrets/wg0.conf";
+      configFile = "/var/mnt/ssd/secrets/wg0.conf";
     };
 
-    # Mounts
-    mounts = {
-      secrets = {
-        enable = true;
-        description = "secrets storage";
-
-        what = "/dev/disk/by-uuid/5137d9ec-d1a6-447f-b69a-fec9939a04d6";
-        where = "/mnt/secrets";
-        options = "subvol=secrets";
-        type = "btrfs";
-
-        wantedBy = [ "multi-user.target" ];
-      };
-    };
   };
 
-  system.stateVersion = "23.05";
+  systemd.services.lighthouse = {
+    enable = true;
+
+    description = "holesky cl";
+    requires = [ "wg-quick-wg0.service" ];
+    after = [ "wg-quick-wg0.service" ];
+
+    script = ''${pkgs.lighthouse}/bin/lighthouse bn \
+      --network holesky \
+      --execution-endpoint http://localhost:8551 \
+      --execution-jwt /var/mnt/ssd/ethereum/holesky/jwt.hex \
+      --checkpoint-sync-url https://holesky.beaconstate.ethstaker.cc/ \
+      --http \
+      --datadir /var/mnt/ssd/ethereum/holesky/lighthouse
+    '';
+
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services.geth = {
+    enable = true;
+
+    description = "holesky el";
+    requires = [ "wg-quick-wg0.service" ];
+    after = [ "wg-quick-wg0.service" ];
+
+    script = ''${pkgs.geth}/bin/geth \
+      --datadir /var/mnt/ssd/ethereum/holesky/geth \
+      --http --http.api="engine,eth,web3,net,debug" \
+      --ws --ws.api="engine,eth,web3,net,debug" \
+      --http.corsdomain "*" \
+      --holesky \
+      --syncmode=full \
+      --authrpc.jwtsecret=/var/mnt/ssd/ethereum/holesky/jwt.hex
+    '';
+
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    9001
+    30303
+  ];
+  networking.firewall.allowedUDPPorts = [
+    9001
+    30303
+  ];
+
+  system.stateVersion = "24.05";
 }
