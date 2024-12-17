1aa68381a81beae13acdfb685bfb008ca13dbd2b
Juuso Haavisto <juuso@ponkila.com>
2024-12-17 01:56:39 +0000

    ponkila-ephemeral-beta: remove testnet

--- nixosConfigurations/ponkila-ephemeral-beta/default.nix
+++ nixosConfigurations/ponkila-ephemeral-beta/default.nix
@@ -36,6 +36,7 @@ in
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkfHIgiK8S5awFn+oOdduS2mp5UGT4ki/ndoMArBol1dvRSKAdHS4okCX/umiy4BqAsDFkpYWuwe897NdOosba0iVyrFsYRou9FrOnQIMRIgtAvaOXeo2U4432glzH4WsMD+D+F4wHZ7walsrkaIPihpoHtWp8DkTPcFm1D8GP1o5TNpTjSFSuPFSzC2nburVcyfxZJluh/hxnxtYLNrmwOOHLhXcTmy5rQQ5u2HI5y64tS6fnKxxozA2gPaVro5+W5e3WtpSDGdd2NkPDzrMMmwYFEv4Tw9ooUfaJhXhq7AJakK/nTfpLquL9XSia8af+aOzx/p1v25f56dESlhNzcSlREP52hTA9T3foCA2IBkDitBeeGhUeeerQdczoRFxxSjoI244bPwAZ+tKIwO0XFaxLyd3jjzlya0F9w1N7wN0ZO4hY1NVv7oaYTUcU7TnvqGEMGLZpQBnIn7DCrUjKeW4AIUGvxcCP+F16lqFkuLSCgOAHM59NECVwBAOPGDk="
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdbU8l66hVUAqk900GmEme5uhWcs05JMUQv2eD0j7MI juuso@starlabs"
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOdsfK46X5IhxxEy81am6A8YnHo2rcF2qZ75cHOKG7ToAAAACHNzaDprYXJp ssh:kari"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILn/9IHTGC1sLxnPnLbtJpvF7HgXQ8xNkRwSLq8ay8eJAAAADHNzaDpzdGFybGFicw== ssh:starlabs"
       ];
       privateKeyFile = sshKeysPath;
     };
@@ -145,59 +146,6 @@ in
     wantedBy = [ "multi-user.target" ];
   };
 
-
-  systemd.services.besu-holesky = {
-    enable = true;
-
-    description = "holesky el";
-    requires = [ "wg-quick-wg0.service" ];
-    after = [ "wg-quick-wg0.service" ];
-
-    script = ''${pkgs.besu}/bin/besu \
-      --network=holesky \
-      --rpc-http-enabled=true \
-      --rpc-http-host=127.0.0.1 \
-      --rpc-http-cors-origins="*" \
-      --rpc-ws-enabled=true \
-      --rpc-ws-host=127.0.0.1 \
-      --host-allowlist="*" \
-      --engine-host-allowlist="*" \
-      --engine-rpc-enabled \
-      --engine-jwt-secret=${config.age.secrets."holesky-jwt".path} \
-      --data-path=/var/mnt/xfs/besu/holesky \
-      --nat-method=upnp \
-      --p2p-port=30342 \
-      --sync-mode=SNAP \
-      --engine-rpc-port=8424 \
-      --rpc-http-port=8542 \
-      --rpc-ws-port=8543 \
-      --metrics-enabled \
-      --metrics-port=9542
-    '';
-
-    wantedBy = [ "multi-user.target" ];
-  };
-
-  systemd.services.lighthouse-holesky = {
-    enable = true;
-
-    description = "holesky cl";
-    requires = [ "wg-quick-wg0.service" ];
-    after = [ "wg-quick-wg0.service" ];
-
-    script = ''${pkgs.lighthouse}/bin/lighthouse bn \
-      --network holesky \
-      --execution-endpoint http://localhost:8424 \
-      --execution-jwt ${config.age.secrets."holesky-jwt".path} \
-      --checkpoint-sync-url https://holesky.beaconstate.ethstaker.cc/ \
-      --http \
-      --datadir /var/mnt/xfs/lighthouse/holesky \
-      --port 9424
-    '';
-
-    wantedBy = [ "multi-user.target" ];
-  };
-
   systemd.network = {
     enable = true;
     networks = {
@@ -213,8 +161,8 @@ in
   };
   networking = {
     firewall = {
-      allowedTCPPorts = [ 50001 30303 30342 9424 8546 ];
-      allowedUDPPorts = [ 50001 30303 30342 9424 8546 51821 ];
+      allowedTCPPorts = [ 50001 30303 8546 ];
+      allowedUDPPorts = [ 50001 30303 8546 51821 ];
     };
     useDHCP = false;
   };
@@ -229,10 +177,6 @@ in
     secrets = {
       mainnet-jwt = {
         rekeyFile = ./secrets/agenix/mainnet-jwt.age;
-        generator.script = "jwt";
-      };
-      holesky-jwt = {
-        rekeyFile = ./secrets/agenix/holesky-jwt.age;
         generator.script = "jwt";
       };
     };
@@ -257,8 +201,6 @@ in
       "health_alarm_notify.conf" = config.sops.secrets."netdata/health_alarm_notify.conf".path;
       "go.d/prometheus.conf" = pkgs.writeText "go.d/prometheus.conf" ''
         jobs:
-          - name: besu-holesky
-            url: http://127.0.0.1:9542/metrics
           - name: etcd
             url: http://127.0.0.1:2379/metrics
       '';
